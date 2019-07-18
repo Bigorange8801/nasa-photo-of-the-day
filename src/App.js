86e4cad6dbc4aa3c9989f5cdb4b24d4af43dbb4b
John <cjd8801@gmail.com>
2019-07-18 18:16:32 -0500

    start of styel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import CardContainer from "./components/CardContainer";
+import Button from "./components/Button"
+import { Button } from 'semantic-ui-react'
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
       <CardContainer/>
+      <Button/>
     </div>
   );
 }
--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { Button } from 'semantic-ui-react'
+import Button from "./Button"
 
 import Header from "./Header";
 import Card from "./Card";
@@ -7,26 +9,27 @@ import Axios from "axios";
 
 function CardContainer() {
   const [data, setData] = useState("loading....");
-
+  const [date,setDate] = useState("2019-07-15")
   console.log("before/after", data);
+  
 
-  const numbers = ["1", "2", "3"];
 
   useEffect(() => {
     Axios.get(
-      "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-15"
+      `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`
     )
       .then(res => {
         setData(res.data);
       })
       .catch(err => console.log(err));
-  }, []);
+  }, [date]);
 
   return (
     <div>
-      <h2>Container</h2>
+      <h2 size="big">Container</h2>
       <Header data={data} />
       <Card data={data} />
+      <button onClick={()=> setDate('2019-07-18')}></button>
     </div>
   );
 }
